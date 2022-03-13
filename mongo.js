e0226c23f79c59f14ed2019af962ad1ef34cc6f9
NPraglin <npraglin@gmail.com>
2022-03-13 16:10:45 -0500

    Updated dependencies

---
+++ mongo.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+// Connection string from MongoDB
+const url = 'mongodb+srv://adminnay:adminnay@npraglin1.2scbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const createProduct = async (req, res, next) => {
+
+};
+
+const getProducts = async (req, res, next) => {
+
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
